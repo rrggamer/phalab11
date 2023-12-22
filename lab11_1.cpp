8b3a351557cdf0c727760af1a2a80c87d59db253
Your Name <you@example.com>
2023-12-22 14:24:25 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+string randomgrade() {
+    int RanIndex = rand() % 9;
+    return grade[RanIndex];
+}
+
+int main() {
+    srand(time(0)); 
+    int i = 0;
+    cout << "Press Enter 3 times to reveal your future.";
+    do {
+        cin.get();
+        i++;
+    } while (i < 3);
+
+    cout << "You will get " << randomgrade() << " in this 261102." << endl;
+
+    return 0;
+}
